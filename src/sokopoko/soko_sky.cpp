b02c5306569b5e6e5be757d5896144d757aec5a1
mechanicalruby <robbierocket05@gmail.com>
2025-06-29 13:31:16 -0700

    remove superfluous texture member from sky

--- src/sokopoko/soko_sky.cpp
+++ src/sokopoko/soko_sky.cpp
@@ -7,7 +7,6 @@ void Sky::initialize(Texture* texture) {
     
     batch.initialize();
     batch.texture = texture;
-    this->texture = texture;
     size = {400, 240};
     uv   = {167, 0, 4, 4};
     top_color.color = 0x00000000;
@@ -16,10 +15,10 @@ void Sky::initialize(Texture* texture) {
 
 void Sky::render() {
     batch.begin();
-    batch.vertices[0] = Vertex{0     , 0     , -1.0f, uv.x / texture->width,               uv.y / texture->height,               top_color.color};
-    batch.vertices[1] = Vertex{0     , size.y, -1.0f, uv.x / texture->width,               (uv.y + uv.height) / texture->height, bottom_color.color};
-    batch.vertices[2] = Vertex{size.x, size.y, -1.0f, (uv.x + uv.width) / texture->width,  (uv.y + uv.height) / texture->height, bottom_color.color};
-    batch.vertices[3] = Vertex{size.x, 0     , -1.0f, (uv.x + uv.width) / texture->width,  uv.y / texture->height,               top_color.color};
+    batch.vertices[0] = Vertex{0     , 0     , -1.0f, uv.x / batch.texture->width,               uv.y / batch.texture->height,               top_color.color};
+    batch.vertices[1] = Vertex{0     , size.y, -1.0f, uv.x / batch.texture->width,               (uv.y + uv.height) / batch.texture->height, bottom_color.color};
+    batch.vertices[2] = Vertex{size.x, size.y, -1.0f, (uv.x + uv.width) / batch.texture->width,  (uv.y + uv.height) / batch.texture->height, bottom_color.color};
+    batch.vertices[3] = Vertex{size.x, 0     , -1.0f, (uv.x + uv.width) / batch.texture->width,  uv.y / batch.texture->height,               top_color.color};
     batch.vertices[4] = batch.vertices[2];
     batch.vertices[5] = batch.vertices[0];
     batch.index_offset = 6;
